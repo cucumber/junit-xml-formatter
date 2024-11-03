8d01a0fa395dca34a6667dff3374c6031ddf48a4
David Goss <david@davidgoss.co>
2024-11-03 00:26:58 +0000

    split some functions out

--- javascript/src/index.ts
+++ javascript/src/index.ts
@@ -1,22 +1,8 @@
-import * as assert from 'node:assert'
-
-import { Envelope, TestCaseStarted, TestStepResultStatus } from '@cucumber/messages'
+import { Envelope } from '@cucumber/messages'
 import xmlbuilder from 'xmlbuilder'
 
 import { ExtendedQuery } from './ExtendedQuery.js'
-import { countStatuses, durationToSeconds, formatStep } from './helpers.js'
-import {
-  namingStrategy,
-  NamingStrategyExampleName,
-  NamingStrategyFeatureName,
-  NamingStrategyLength,
-} from './Lineage.js'
-
-const NAMING_STRATEGY = namingStrategy(
-  NamingStrategyLength.LONG,
-  NamingStrategyFeatureName.EXCLUDE,
-  NamingStrategyExampleName.NUMBER_AND_PICKLE_IF_PARAMETERIZED
-)
+import { makeReport } from './makeReport.js'
 
 export default {
   type: 'formatter',
@@ -27,16 +13,16 @@ export default {
     on: (type: 'message', handler: (message: Envelope) => void) => void
     write: (content: string) => void
   }) {
-    const cucumberQuery = new ExtendedQuery()
+    const query = new ExtendedQuery()
     const builder = xmlbuilder
       .create('testsuite', { invalidCharReplacement: '' })
       .att('name', 'Cucumber')
 
     on('message', (message) => {
-      cucumberQuery.update(message)
+      query.update(message)
 
       if (message.testRunFinished) {
-        const testSuite = makeReport(cucumberQuery)
+        const testSuite = makeReport(query)
         builder.att('time', testSuite.time)
         builder.att('tests', testSuite.tests)
         builder.att('skipped', testSuite.skipped)
@@ -68,86 +54,4 @@ export default {
       }
     })
   },
-}
-
-interface ReportSuite {
-  time: number
-  tests: number
-  skipped: number
-  failures: number
-  errors: number
-  testCases: ReadonlyArray<ReportTestCase>
-}
-
-interface ReportTestCase {
-  classname: string
-  name: string
-  time: number
-  failure?: ReportFailure
-  output: string
-}
-
-interface ReportFailure {
-  kind: 'failure' | 'skipped'
-  type?: string
-  message?: string
-  stack?: string
-}
-
-function makeReport(query: ExtendedQuery): ReportSuite {
-  const statuses = query.countMostSevereTestStepResultStatus()
-  return {
-    time: durationToSeconds(query.findTestRunDuration()),
-    tests: countStatuses(statuses),
-    skipped: countStatuses(statuses, (status) => status === TestStepResultStatus.SKIPPED),
-    failures: countStatuses(
-      statuses,
-      (status) => status !== TestStepResultStatus.PASSED && status !== TestStepResultStatus.SKIPPED
-    ),
-    errors: 0,
-    testCases: makeTestCases(query),
-  }
-}
-
-function makeTestCases(query: ExtendedQuery): ReadonlyArray<ReportTestCase> {
-  return query.findAllTestCaseStarted().map((testCaseStarted) => {
-    const pickle = query.findPickleBy(testCaseStarted)
-    assert.ok(pickle, 'Expected to find Pickle by TestCaseStarted')
-    const lineage = query.findLineageBy(pickle)
-
-    return {
-      classname: lineage?.feature?.name ?? pickle.uri,
-      name: query.findNameOf(pickle, NAMING_STRATEGY),
-      time: durationToSeconds(query.findTestCaseDurationBy(testCaseStarted)),
-      failure: makeFailure(query, testCaseStarted),
-      output: query
-        .findTestStepFinishedAndTestStepBy(testCaseStarted)
-        // filter out hooks
-        .filter(([, testStep]) => !!testStep.pickleStepId)
-        .map(([testStepFinished, testStep]) => {
-          const pickleStep = query.findPickleStepBy(testStep)
-          assert.ok(pickleStep, 'Expected to find PickleStep by TestStep')
-          const gherkinStep = query.findStepBy(pickleStep)
-          assert.ok(gherkinStep, 'Expected to find Step by PickleStep')
-          return formatStep(gherkinStep, pickleStep, testStepFinished.testStepResult.status)
-        })
-        .join('\n'),
-    }
-  })
-}
-
-function makeFailure(
-  query: ExtendedQuery,
-  testCaseStarted: TestCaseStarted
-): ReportFailure | undefined {
-  const result = query.findMostSevereTestStepResultBy(testCaseStarted)
-  if (result.status === TestStepResultStatus.PASSED) {
-    return undefined
-  }
-  return {
-    kind: result.status === TestStepResultStatus.SKIPPED ? 'skipped' : 'failure',
-    type: result.exception?.type,
-    message: result.exception?.message,
-    stack: result.exception?.stackTrace ?? result.message,
-  }
 }
---
+++ javascript/src/makeReport.ts
@@ -0,0 +1,100 @@
+import assert from 'node:assert'
+
+import { TestCaseStarted, TestStepResultStatus } from '@cucumber/messages'
+
+import { ExtendedQuery } from './ExtendedQuery.js'
+import { countStatuses, durationToSeconds, formatStep } from './helpers.js'
+import {
+  namingStrategy,
+  NamingStrategyExampleName,
+  NamingStrategyFeatureName,
+  NamingStrategyLength,
+} from './Lineage.js'
+
+const NAMING_STRATEGY = namingStrategy(
+  NamingStrategyLength.LONG,
+  NamingStrategyFeatureName.EXCLUDE,
+  NamingStrategyExampleName.NUMBER_AND_PICKLE_IF_PARAMETERIZED
+)
+
+interface ReportSuite {
+  time: number
+  tests: number
+  skipped: number
+  failures: number
+  errors: number
+  testCases: ReadonlyArray<ReportTestCase>
+}
+
+interface ReportTestCase {
+  classname: string
+  name: string
+  time: number
+  failure?: ReportFailure
+  output: string
+}
+
+interface ReportFailure {
+  kind: 'failure' | 'skipped'
+  type?: string
+  message?: string
+  stack?: string
+}
+
+export function makeReport(query: ExtendedQuery): ReportSuite {
+  const statuses = query.countMostSevereTestStepResultStatus()
+  return {
+    time: durationToSeconds(query.findTestRunDuration()),
+    tests: countStatuses(statuses),
+    skipped: countStatuses(statuses, (status) => status === TestStepResultStatus.SKIPPED),
+    failures: countStatuses(
+      statuses,
+      (status) => status !== TestStepResultStatus.PASSED && status !== TestStepResultStatus.SKIPPED
+    ),
+    errors: 0,
+    testCases: makeTestCases(query),
+  }
+}
+
+function makeTestCases(query: ExtendedQuery): ReadonlyArray<ReportTestCase> {
+  return query.findAllTestCaseStarted().map((testCaseStarted) => {
+    const pickle = query.findPickleBy(testCaseStarted)
+    assert.ok(pickle, 'Expected to find Pickle by TestCaseStarted')
+    const lineage = query.findLineageBy(pickle)
+
+    return {
+      classname: lineage?.feature?.name ?? pickle.uri,
+      name: query.findNameOf(pickle, NAMING_STRATEGY),
+      time: durationToSeconds(query.findTestCaseDurationBy(testCaseStarted)),
+      failure: makeFailure(query, testCaseStarted),
+      output: query
+        .findTestStepFinishedAndTestStepBy(testCaseStarted)
+        // filter out hooks
+        .filter(([, testStep]) => !!testStep.pickleStepId)
+        .map(([testStepFinished, testStep]) => {
+          const pickleStep = query.findPickleStepBy(testStep)
+          assert.ok(pickleStep, 'Expected to find PickleStep by TestStep')
+          const gherkinStep = query.findStepBy(pickleStep)
+          assert.ok(gherkinStep, 'Expected to find Step by PickleStep')
+          return formatStep(gherkinStep, pickleStep, testStepFinished.testStepResult.status)
+        })
+        .join('\n'),
+    }
+  })
+}
+
+function makeFailure(
+  query: ExtendedQuery,
+  testCaseStarted: TestCaseStarted
+): ReportFailure | undefined {
+  const result = query.findMostSevereTestStepResultBy(testCaseStarted)
+  if (result.status === TestStepResultStatus.PASSED) {
+    return undefined
+  }
+  return {
+    kind: result.status === TestStepResultStatus.SKIPPED ? 'skipped' : 'failure',
+    type: result.exception?.type,
+    message: result.exception?.message,
+    stack: result.exception?.stackTrace ?? result.message,
+  }
+}
