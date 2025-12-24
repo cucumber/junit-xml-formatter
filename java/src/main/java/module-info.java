ca1a41810a7850492c67409fa2af39a0873e5600
M.P. Korstanje <rien.korstanje@gmail.com>
2025-12-24 18:30:23 +0100

    Introduce module

---
+++ java/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module io.cucumber.junitxmlformatter {
+    requires org.jspecify;
+
+    requires java.xml;
+
+    requires io.cucumber.messages;
+    requires io.cucumber.query;
+
+    exports io.cucumber.junitxmlformatter;
+}
